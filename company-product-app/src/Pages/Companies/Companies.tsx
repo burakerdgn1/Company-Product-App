6d1477aab401700fe5a0e6adbbe7cd3caf3531e9
burakerdgn1 <burakerdogan749@gmail.com>
2023-09-20 13:53:15 +0300

    Companies page is updated.

--- company-product-app/src/Pages/Companies/Companies.tsx
+++ company-product-app/src/Pages/Companies/Companies.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Table, Button, Input, Form, Space, Modal } from 'antd';
-import { EditOutlined, DeleteOutlined ,SearchOutlined,ExclamationCircleOutlined} from '@ant-design/icons';
+import { EditOutlined, DeleteOutlined, SearchOutlined, ExclamationCircleOutlined, PlusOutlined } from '@ant-design/icons';
+//import { FilterDropdownProps } from 'antd/es/table/interface';
 
 const { Column } = Table;
 const { Search } = Input;
@@ -32,133 +33,187 @@ function Companies() {
         legalNumber: string;
         country: string;
         website: string;
-      }
-    
+    }
+
 
     const [companies, setCompanies] = useState<Company[]>(dummyCompanies);
-    const [searchText, setSearchText] = useState('');
-    const [searchedColumn, setSearchedColumn] = useState('');
+    const [searchText, setSearchText] = useState<string>('');
+    const [searchedColumn, setSearchedColumn] = useState<string>('');
+
+    const [isAddCompanyVisible, setIsAddCompanyVisible] = useState(false);
+
+    const toggleAddCompanyForm = () => {
+        setIsAddCompanyVisible(!isAddCompanyVisible);
+    };
+
+    const formLayout = {
+        labelCol: { span: 6 },
+        wrapperCol: { span: 18 },
+    };
+
     const [form] = Form.useForm();
 
-    const handleAddCompany = (values:any) => {
-        const newCompany = {
-          key: (companies.length + 1).toString(), // Generate a unique key
-          ...values,
-        };
-        setCompanies([...companies, newCompany]);
-        form.resetFields();
-      };
+    const handleAddCompany = (values: any) => {
+        form
+            .validateFields()
+            .then((values) => {
+                const newCompany = {
+                    key: (companies.length + 1).toString(),
+                    ...values,
+                };
+                setCompanies([...companies, newCompany]);
+                form.resetFields();
+                setIsAddCompanyVisible(false);
+            })
+            .catch((errorInfo) => {
+                console.log('Validation failed:', errorInfo);
+            });
+    };
 
-      const [editingCompany, setEditingCompany] = useState<Company|null>(null);
+    const [editingCompany, setEditingCompany] = useState<Company | null>(null);
 
-      const handleEditCompany = (values:any) => {
-        if(editingCompany){
+    const handleEditCompany = (values: any) => {
+        if (editingCompany) {
             const updatedCompanies = companies.map((company) =>
-          company.key === editingCompany.key ? { ...company, ...values } : company
-        );
-        setCompanies(updatedCompanies);
-        setEditingCompany(null);
-        form.resetFields();
+                company.key === editingCompany.key ? { ...company, ...values } : company
+            );
+            setCompanies(updatedCompanies);
+            setEditingCompany(null);
+            form.resetFields();
 
         }
-        
-      };
 
-      const showDeleteConfirm = (record:Company) => {
+    };
+
+    const showDeleteConfirm = (record: Company) => {
         confirm({
-          title: 'Are you sure you want to delete this company?',
-          icon: <ExclamationCircleOutlined />,
-          okText: 'Yes',
-          okType: 'danger',
-          cancelText: 'No',
-          onOk() {
-            handleDeleteCompany(record.key);
-          },
-          onCancel() {
-            console.log('Cancel');
-          },
+            title: 'Are you sure you want to delete this company?',
+            icon: <ExclamationCircleOutlined />,
+            okText: 'Yes',
+            okType: 'danger',
+            cancelText: 'No',
+            onOk() {
+                handleDeleteCompany(record.key);
+            },
+            onCancel() {
+                console.log('Cancel');
+            },
         });
-      };
+    };
 
-      const handleDeleteCompany = (key:string) => {
+    const handleDeleteCompany = (key: string) => {
         const updatedCompanies = companies.filter((company) => company.key !== key);
         setCompanies(updatedCompanies);
-      };
+    };
 
-      const handleSearch = (selectedKeys:string[]|null, confirm:(()=>void)|null, dataIndex:string) => {
-        if(confirm){
+    const handleSearch = (selectedKeys: string[] | null, confirm: (() => void) | null, dataIndex: string) => {
+        if (confirm) {
             confirm();
         }
-        setSearchText(selectedKeys?selectedKeys[0]:'');
+        setSearchText(selectedKeys ? selectedKeys[0] : '');
         setSearchedColumn(dataIndex);
-      };
+    };
 
-      const handleReset = (clearFilters:()=>void) => {
+    const handleReset = (clearFilters: () => void) => {
         clearFilters();
         setSearchText('');
-      };
+    };
 
-      const getColumnSearchProps = (dataIndex:string) => ({
-        filterDropdown: ( setSelectedKeys:(selectedKeys:string[])=>{}, selectedKeys:string[], confirm:()=>{}, clearFilters:()=>{} ) => (
-          <div style={{ padding: 8 }}>
-            <Input
-              placeholder={`Search ${dataIndex}`}
-              value={selectedKeys[0]}
-              onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-              onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-              style={{ width: 188, marginBottom: 8, display: 'block' }}
-            />
-            <Space>
-              <Button
-                type="primary"
-                onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-                icon={<SearchOutlined />}
-                size="small"
-                style={{ width: 90 }}
-              >
-                Search
-              </Button>
-              <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
-                Reset
-              </Button>
-            </Space>
-          </div>
-        ),
-        filterIcon: (filtered:boolean) => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
-        onFilter: (value:string, record:Company) =>
-          record[dataIndex as keyof Company] ? record[dataIndex as keyof Company].toString().toLowerCase().includes(value.toLowerCase()) : '',
-        render: (text:string) =>
-          searchedColumn === dataIndex ? (
-            <Button type="link" onClick={() => handleSearch(null, null, dataIndex)}>
-              {text}
-            </Button>
-          ) : (
-            text
-          ),
-      });
+    // interface CustomFilterDropdownProps {
+    //     setSelectedKeys: (selectedKeys: React.Key[]) => void;
+    //     selectedKeys: React.Key[];
+    //     confirm: () => void;
+    //     clearFilters: () => void;
+    //   }
 
-      const urlPattern = /^(https?:\/\/)?([\w-]+(\.[\w-]+)+\/?)?([\w-]+\/?)*\??([\w-]+=[\w-]*(\&[\w-]+=[\w-]*)*)?$/;
+    // const getColumnSearchProps = (
+    //     dataIndex: string
+    //   ): {
+    //     filterDropdown: (props: CustomFilterDropdownProps) => React.ReactNode;
+    //     filterIcon: (filtered: boolean) => React.ReactNode;
+    //     onFilter: (value: string, record: Company) => boolean;
+    //     render: (text: string) => React.ReactNode;
+    //   } => ({
+    //     filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: CustomFilterDropdownProps) => (
+    //       <div style={{ padding: 8 }}>
+    //         <Input
+    //           placeholder={`Search ${dataIndex}`}
+    //           value={selectedKeys[0] as string}
+    //           onChange={(e) => setSelectedKeys([e.target.value])}
+    //           onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+    //           style={{ width: 188, marginBottom: 8, display: 'block' }}
+    //         />
+    //         <Space>
+    //           <Button
+    //             type="primary"
+    //             onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+    //             icon={<SearchOutlined />}
+    //             size="small"
+    //             style={{ width: 90 }}
+    //           >
+    //             Search
+    //           </Button>
+    //           <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+    //             Reset
+    //           </Button>
+    //         </Space>
+    //       </div>
+    //     ),
+    //     filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    //     onFilter: (value: string, record: Company) =>
+    //       record[dataIndex as keyof Company]
+    //         ? record[dataIndex as keyof Company].toString().toLowerCase().includes(value.toLowerCase())
+    //         : false,
+    //     render: (text: string) =>
+    //       searchedColumn === dataIndex ? (
+    //         <Button type="link" onClick={() => handleSearch(null, null, dataIndex)}>
+    //           {text}
+    //         </Button>
+    //       ) : (
+    //         text
+    //       ),
+    //   });
+
+    // const companyNameColumnSearchProps = getColumnSearchProps('companyName');
+
+
+    const urlPattern = /^(https?:\/\/)?([\w-]+(\.[\w-]+)+\/?)?([\w-]+\/?)*\??([\w-]+=[\w-]*(\&[\w-]+=[\w-]*)*)?$/;
 
 
     return (
         <div>
             <h2>Companies</h2>
-            <Space style={{ marginBottom: 16 }}>
+            {/* <Space style={{ marginBottom: 16 }}>
                 <Search
                     placeholder="Search by company name"
                     allowClear
                     enterButton
                     onSearch={(value) => setSearchText(value)}
                 />
-            </Space>
+            </Space> */}
+            <Button type="primary" icon={<PlusOutlined />} onClick={toggleAddCompanyForm}>
+                Add Company
+            </Button>
             <Table dataSource={companies} size="middle" bordered>
                 <Column
                     title="Company Name"
                     dataIndex="companyName"
-
-                    //check here
                     key="companyName"
-                    //{...getColumnSearchProps('companyName')}
+
+                //will be checked later
+
+                // filterDropdown={(props: FilterDropdownProps) =>
+                //     companyNameColumnSearchProps.filterDropdown({
+                //         ...props,
+                //         setSelectedKeys: props.setSelectedKeys as (selectedKeys: string[]) => void,
+                //         selectedKeys: props.selectedKeys as string[],
+                //         confirm: props.confirm as () => void,
+                //         clearFilters: props.clearFilters as () => void,
+                //     })
+                // }
+                // filterIcon={companyNameColumnSearchProps.filterIcon}
+                // onFilter={companyNameColumnSearchProps.onFilter}
+                // render={companyNameColumnSearchProps.render}
                 />
                 <Column title="Legal Number" dataIndex="legalNumber" key="legalNumber" />
                 <Column title="Incorporation Country" dataIndex="country" key="country" />
@@ -166,7 +221,7 @@ function Companies() {
                 <Column
                     title="Actions"
                     key="actions"
-                    render={(_, record:Company) => (
+                    render={(_, record: Company) => (
                         <Space size="middle">
                             <Button
                                 type="primary"
@@ -240,7 +295,7 @@ function Companies() {
                                 },
                                 {
                                     type: 'url',
-                                    pattern:urlPattern,
+                                    pattern: urlPattern,
                                     message: 'Please enter a valid URL',
                                 },
                             ]}
@@ -251,12 +306,88 @@ function Companies() {
 
                         <Form.Item>
                             <Button type="primary" htmlType="submit">
-                                {editingCompany?'Save':'Add'}
+                                {editingCompany ? 'Save' : 'Add'}
                             </Button>
                         </Form.Item>
                     </Form>
                 </>
             )}
+
+            <Modal
+                title="Add Company"
+                visible={isAddCompanyVisible}
+                onCancel={toggleAddCompanyForm}
+                footer={null}
+            >
+                <Form form={form} {...formLayout}>
+                    <Form.Item
+                        name="companyName"
+                        label="Company Name"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter the company name',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Company Name" />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="legalNumber"
+                        label="Legal Number"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter the legal number',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Legal Number" />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="country"
+                        label="Incorporation Country"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter the incorporation country',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Incorporation Country" />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="website"
+                        label="Website"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter the company website',
+                            },
+                            {
+                                type: 'url',
+                                message: 'Please enter a valid URL',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Website" />
+                    </Form.Item>
+
+                    <Form.Item wrapperCol={{ ...formLayout.wrapperCol, offset: 6 }}>
+                        <Space>
+                            <Button onClick={toggleAddCompanyForm}>Cancel</Button>
+                            <Button type="primary" onClick={handleAddCompany}>
+                                Add
+                            </Button>
+                        </Space>
+                    </Form.Item>
+                </Form>
+            </Modal>
+
+
         </div>
     );
 }
